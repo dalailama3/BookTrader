869ce2baa54edcdcefd9cc8ad984cf99b100086a
John Clark <johngclark36@gmail.com>
2017-03-01 17:11:08 -0500

    Include jquery cdn in userBooks.ejs, change dom javascript to jquery manipulation, create radio-option divs for trading books

--- app/controllers/userBooks.client.js
+++ app/controllers/userBooks.client.js
@@ -1,57 +1,103 @@
 'use strict';
 
-(function () {
 
-  var books = document.querySelector('.books').innerHTML
-  var userBooks = document.querySelector('.curUserBooks').innerHTML
+  $(document).ready(function () {
+    var curUserList = []
+    var otherUserList = []
+    var books = $('.books').text()
+    var userBooks = $('.curUserBooks').text()
 
-  books = JSON.parse(books)
-  userBooks = JSON.parse(userBooks)
+    books = JSON.parse(books)
+    userBooks = JSON.parse(userBooks)
 
-  function listBooks (data) {
-    var data = JSON.parse(data)
-    var booksUl = document.querySelector('.bookshelf')
-    var rightUl = document.querySelector('.bookshelf-otherUser-list')
-
-    var li = document.createElement("li")
-    var rightLi = document.createElement("li")
-    rightLi.innerHTML = data.volumeInfo.title
-
-    var img = document.createElement("img")
-    img.setAttribute('src', data.volumeInfo.imageLinks.smallThumbnail)
-    li.append(img)
-
-    booksUl.append(li)
-    rightUl.append(rightLi)
-
-  }
-
-  function listCurUserBooks (data) {
-    var data = JSON.parse(data)
-    var leftUl = document.querySelector('.bookshelf-curUser-list')
-    var leftLi = document.createElement("li")
-    leftLi.innerHTML = data.volumeInfo.title
-    leftUl.append(leftLi)
-  }
-
-  books.forEach((bookId)=> {
-    var booksUrl = `https://www.googleapis.com/books/v1/volumes/${bookId}?key=AIzaSyABcubhr0IIhjhjYwOQGvzeAMnXy2SK-Hg`
-    ajaxFunctions.ajaxRequest('GET', booksUrl, listBooks);
-  })
-
-  userBooks.forEach((bookId)=> {
-    var booksUrl = `https://www.googleapis.com/books/v1/volumes/${bookId}?key=AIzaSyABcubhr0IIhjhjYwOQGvzeAMnXy2SK-Hg`
-    ajaxFunctions.ajaxRequest('GET', booksUrl, listCurUserBooks);
-  })
-
-  var tradeView = document.querySelector('.trade-view')
-  var tradeButton = document.querySelector('.trade')
-  tradeButton.addEventListener('click', function (event) {
-
-  })
+    var $booksUl = $('.bookshelf')
+    var $rightUl = $('.bookshelf-otherUser-list')
 
 
+    function listBooks (data) {
+
+      var data = JSON.parse(data)
+      var $li = $("<li>")
+      var $rightLi = $("<li>", { 'class': 'trade' })
+      $rightLi.text(data.volumeInfo.title)
+
+      var $img = $("<img>", {
+        'src': data.volumeInfo.imageLinks.smallThumbnail
+      })
+      var $radioDiv = $('<div>', { 'class': 'radio-option'})
+      $radioDiv.attr('data-bookId', data.id)
+      $radioDiv.on("click", function () {
+        $(this).not(this).removeClass('click');
+        $(this).toggleClass("click");
+      })
 
 
+      $li.append($img)
+      $rightLi.append($radioDiv)
+      $booksUl.append($li)
+      $rightUl.append($rightLi)
 
-})();
+
+    }
+
+    function listCurUserBooks (data) {
+      var data = JSON.parse(data)
+      var $leftUl = $('.bookshelf-curUser-list')
+      var $leftLi = $("<li>", { 'class': 'trade'})
+      var $radioDiv = $('<div>', { 'class': 'radio-option'})
+      $radioDiv.attr('data-bookId', data.id)
+
+      $radioDiv.on("click", function () {
+        $(this).not(this).removeClass('click');
+        $(this).toggleClass("click");
+
+      })
+
+
+      $leftLi.text(data.volumeInfo.title)
+      $radioDiv.appendTo($leftLi)
+      $leftUl.append($leftLi)
+
+
+    }
+
+
+      var tradeButton = $('button.trade')
+
+      tradeButton.on("click", function () {
+
+        var clicked = $('.bookshelf-curUser-list .radio-option.click')
+        var clicked = Array.prototype.slice.call(clicked)
+        var arr = []
+        clicked.forEach((el)=> {
+          arr.push($(el).attr('data-bookId'))
+        })
+
+        curUserList = arr
+        console.log(curUserList)
+
+        var otherClicked = $('.bookshelf-otherUser-list .radio-option.click')
+        var otherClicked = Array.prototype.slice.call(otherClicked)
+
+        var otherArr = []
+        otherClicked.forEach((el)=> {
+          otherArr.push($(el).attr('data-bookId'))
+        })
+
+        otherUserList = otherArr
+        console.log(otherUserList)
+
+      })
+
+      books.forEach((bookId)=> {
+        var booksUrl = `https://www.googleapis.com/books/v1/volumes/${bookId}?key=AIzaSyABcubhr0IIhjhjYwOQGvzeAMnXy2SK-Hg`
+        ajaxFunctions.ajaxRequest('GET', booksUrl, listBooks);
+      })
+
+      userBooks.forEach((bookId)=> {
+        var booksUrl = `https://www.googleapis.com/books/v1/volumes/${bookId}?key=AIzaSyABcubhr0IIhjhjYwOQGvzeAMnXy2SK-Hg`
+        ajaxFunctions.ajaxRequest('GET', booksUrl, listCurUserBooks);
+      })
+
+
+  });
